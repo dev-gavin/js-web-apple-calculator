ca73689234e86af5122085b27d807954e91cade2
Gavin Meeker <gmeeker99@gmail.com>
2023-11-10 21:33:26 -0500

    feat: numpad works

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ function handleNumClick({ target }) {
     var numPressed = target.textContent.trim()
     var lastItem = currentExpression.at(-1)
 
-    if (!lastItem) {
+    if (currentExpression.length == 0) {
         currentExpression.push(numPressed)
+    } else if (lastItem.length == 7) {
+        return
+    } else if (!Number.isNaN(lastItem) || numPressed == undefined) {
+        currentExpression[currentExpression.length - 1] = lastItem + numPressed
     }
 
-    setScreen(numPressed)
+    lastItem = currentExpression.at(-1)
+
+    setScreen(lastItem)
 }
 
 function setScreen(result) {
@@ -20,4 +26,5 @@ function setScreen(result) {
 
 function handleClear() {
     screen.textContent = 0
+    currentExpression = []
 }
